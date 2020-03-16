10e8cf2c94fcbb78b6e0105eaeaa3c2be372ba89
SeungyoungYang <2014112096@dongguk.edu>
2020-03-16 15:45:05 +0900

    Add api routing

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+
+	"github.com/SeungyoungYang/Go-dev/api"
 )
 
 func main() {
@@ -16,13 +16,8 @@ func main() {
 	e.Use(middleware.Recover())
 
 	// Routes
-	e.GET("/", hello)
+	e.GET("/", api.Index)
 
 	// Start server
 	e.Logger.Fatal(e.Start(":1323"))
-}
-
-// Handler
-func hello(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello, World!")
 }
